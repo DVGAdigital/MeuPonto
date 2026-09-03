cf55092cb3e184958ce036fbc02667c32daf0e5e
DVGAdigital <dvgadigital@gmail.com>
2026-09-03 12:45:32 -0300

    Criar server.js

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("Meu Ponto - servidor online");
+});
+
+app.post("/webhook", (req, res) => {
+    console.log("Webhook recebido:", req.body);
+
+    res.sendStatus(200);
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
